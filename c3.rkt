9d5a9b2dde28ff4d61a388099268de6d07b483f8
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-05-10 04:02:53 +0700

    This time is 4:02 AM, I need to go home to sleep

---
+++ c3.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(zero? 1)
+(zero? (sub1 1))
+(= 0 1)
+(= 0 0)
+;(= (zero? 1) 
+;   (= 0 1))
+(symbol=? 'foo 'FOO)
+(expt 53 53)
+(sqrt -1)
+(* (sqrt -1) (sqrt -1))
+(/ 4 6)
+(/ 4.0 6)
+(string-append "tutti" "frutti")
+(string-append "tutti" " " "frutti")
+(cons 1 2)
+(define cell (cons 'a 'b))
+(cons 'chicken empty)
+
+
+
+
