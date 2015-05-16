fc5aac3dc1c2fc7e2cc68213cbf8f13bdaa06695
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-05-17 04:43:27 +0700

    forgot save

--- euler/002.rkt
+++ euler/002.rkt
@@ -1,1 +1,20 @@
 #lang racket
+ 
+(require rackunit)
+
+(define (solve)
+  (define (fib-sum upto filter)
+    (define (h i j acc)
+      (if (> i upto) acc
+          (h j (+ i j) (if (filter i) (+ i acc) acc))))
+    (h 1 2 0))
+  (fib-sum 4000000 even?))
+
+(check-equal? (solve) 4613732)
+
+; test tail recursive
+(define (jrect i j acc)
+  (if (> i 10) acc
+      (jrect j (+ i j) (if (even? i) (+ i acc) acc))))
+
+(check-equal? (jrect 1 2 0) (+ 2 3 5))
