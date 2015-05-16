a31328965872c8c806a303f530a554fd77cf41d8
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-05-16 21:36:12 +0700

    start ch4

--- c3.rkt
+++ c3.rkt
@@ -62,7 +62,7 @@
 (student-id# freshman1)
 
 (define mini (student 'Mini 1234 'NewHall))
-(define nicole (student 'Nicole 5678 'Newhall))
+(define nicole (student 'Nicole 5678 'NewHall))
 (define rose (student 'Rose 8765 'NewHall))
 (define eric (student 'Eric 4321 'NewHall))
 (define in-class (list mini nicole rose eric))
